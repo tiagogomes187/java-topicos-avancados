a977468ab634c5777fae29497356edd191dce5c9
Tiago Gomes <tiagogomes187@gmail.com>
2023-12-26 22:13:57 -0300

    Métodos readString() e writeString() da classe Files

---
+++ Secao-5-Java-11-New-Features/aula-32/MeuProjetoLambda/src/MinhaClasseLambda.java
@@ -0,0 +1,12 @@
+import java.util.function.Function;
+
+public class MinhaClasseLambda {
+	public static void main(String[] args) {
+
+		String str = "Java Tópicos Avançados";
+
+		Function<String, String> concatena = (var s) -> s + ". ";
+
+
+	}
+}
