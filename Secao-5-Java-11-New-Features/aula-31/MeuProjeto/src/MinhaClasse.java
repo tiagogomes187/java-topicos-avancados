9f552228ca93d8a5aef724b0b9278261ea4052e1
Tiago Gomes <tiagogomes187@gmail.com>
2023-12-26 21:50:01 -0300

    Novos métodos da classe String - isblank(), lines(), repeat

---
+++ Secao-5-Java-11-New-Features/aula-31/MeuProjeto/src/MinhaClasse.java
@@ -0,0 +1,18 @@
+import java.util.stream.Collectors;
+
+public class MinhaClasse {
+	public static void main(String[] args) {
+
+//		String str1 = "          ";
+//		String str2 = "";
+//		String str3 = "Java\n Tópicos\n Avancados.";
+		String str3 = "Java Tópicos Avancados.";
+
+//		System.out.println(str1.isBlank());
+//		System.out.println(str2.isBlank());
+//		System.out.println(str3.isBlank());
+//		System.out.println(str3.lines().collect(Collectors.toList()));
+		System.out.println(str3.repeat(3));
+
+	}
+}
